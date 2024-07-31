74c1afca85d34aeff72a4155d5805ca87d76910d
akintolatech <akintolatechnologies@gmail.com>
2024-08-01 00:05:58 +0100

    export function extension

--- streamlit_res/webrtcai.py
+++ streamlit_res/webrtcai.py
@@ -7,13 +7,16 @@ import streamlit as st
 from streamlit_webrtc import webrtc_streamer, RTCConfiguration, VideoProcessorBase
 import av
 import cv2
+import numpy as np
 
 
 class VideoProcessor(VideoProcessorBase):
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
-        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
+        # img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+        matrix = np.ones(img.shape, dtype="uint8") * 50
+        b_img = cv2.add(img, matrix)
+        return av.VideoFrame.from_ndarray(b_img, format="bgr24")
 
 
 st.title("Facial Attribute Analysis Software")
